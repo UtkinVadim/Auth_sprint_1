ed2c767bf816efce1cedbbf0ba02e07eb68cd946
Alexandr Galanov <alexandr.galanov@gmail.com>
2021-12-18 15:30:10 +0300

    Добавлена проверка sign_in пользователя

--- tests/create_user.sh
+++ tests/create_user.sh
@@ -1,7 +1,12 @@
-LOGIN=`head /dev/urandom |LC_ALL=C  tr -dc A-Za-z0-9 | head -c10`
+LOGIN=$(head /dev/urandom |LC_ALL=C  tr -dc A-Za-z0-9 | head -c10)
 echo ${LOGIN}
 
-USER_INFO="{\"login\": \"${LOGIN}\", \"email\": \"test@test\", \"password\": \"password\"}"
-echo ${USER_INFO}
+USER_CREATE_INFO="{\"login\": \"${LOGIN}\", \"email\": \"test@test\", \"password\": \"password\"}"
+echo ${USER_CREATE_INFO}
+USER_LOGIN_INFO="{\"login\": \"${LOGIN}\", \"email\": \"test@test\", \"password\": \"password\"}"
+echo ${USER_LOGIN_INFO}
 
-curl http://127.0.0.1:5000/api/v1/user/sign_up -XPOST -d "${USER_INFO}" -H 'Content-Type: application/json'
+echo 'trying to create user'
+curl http://127.0.0.1:5000/api/v1/user/sign_up -XPOST -d "${USER_CREATE_INFO}" -H 'Content-Type: application/json'
+echo 'trying to sign_in'
+curl http://127.0.0.1:5000/api/v1/user/sign_in -XPOST -d "${USER_LOGIN_INFO}" -H 'Content-Type: application/json'
