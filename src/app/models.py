ecd622c9a5db0327f8fb5a5f5fb8b0fec721bb3b
Alexandr Galanov <alexandr.galanov@gmail.com>
2021-12-18 13:53:18 +0300

    Изменена структура, добавлены классы с ролями

--- src/app/models.py
+++ src/app/models.py
@@ -1,30 +1,57 @@
 import datetime
 import uuid
 
-from sqlalchemy.dialects.postgresql import UUID, BOOLEAN
+from sqlalchemy.dialects.postgresql import UUID, BOOLEAN, INTEGER
 
 from db import db
 
 
-class UsersPrivateInfoModel(db.Model):
-    __tablename__ = 'users_private_info'
+class Users(db.Model):
+    __tablename__ = 'users'
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     login = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
     created_on = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
+    roles = db.relationship("RolesModel", secondary="user_roles", backref=db.backref("users", lazy="dynamic"))
+    is_active = db.Column(BOOLEAN, default=True)
 
     def __repr__(self):
-        return f'<User {self.login}>'
+        return f'<User {self.login} {self.id}>'
 
 
-class UsersModel(db.Model):
-    __tablename__ = 'users'
+class UserRolesModel(db.Model):
+    __tablename__ = 'user_roles'
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    user_info = db.relationship('UsersPrivateInfoModel', backref='private_info', uselist=False)
-    is_active = db.Column(BOOLEAN)
-    # role_id = db.relationship('Roles', backref='users')
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), unique=True, nullable=False, use_list=False)
+    role_id = db.Column(UUID(as_uuid=True), db.ForeignKey('roles.id'))
+
+
+class RolesModel(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(UUID(as_uuid=True), primary_key=True, unique=True, nullable=False)
+    title = db.Column(db.String(255), unique=True, nullable=False)
+
+
+class LoginHistoryModel(db.Model):
+    __tablename__ = 'login_history'
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=False)
+    fingerprint = db.Column(db.String)
+    event_date = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
+
+
+def create_user(user_fields):
+    """
+    Создаёт пользователя в базе
+
+    :param user_fields:
+    :return:
+    """
+    user = Users(**user_fields)
+    db.session.add(user)
+    db.session.commit()
+    return user
