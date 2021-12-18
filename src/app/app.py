f05206677f1671e5b2c992ae988d5168a5c4328b
Alexandr Galanov <alexandr.galanov@gmail.com>
2021-12-18 14:09:29 +0300

    Строка проверки создания пользователя вынесена отдельно

--- src/app/app.py
+++ src/app/app.py
@@ -27,11 +27,10 @@ def main():
     # подключение ресурсов, можно перенести в api/v1/users
     api_app.add_resource(api.v1.user.UserInfo, url_generator(postfix='user_info'))
     api_app.add_resource(api.v1.user.UserRegistration, url_generator(postfix='user'))
+
     api_app.add_resource(api.v1.user.UserSignUp, url_generator(postfix='user/sign_up'))
     app.run()
 
 
 if __name__ == '__main__':
     main()
-
-# curl  http://127.0.0.1:5000/api/v1/user/sign_up -XPOST -d '{"login": "test", "email": "test@test", "password": "password"}' -H 'Content-Type: application/json'
