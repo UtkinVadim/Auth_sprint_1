fa905f03b66f3793f117d77debf590c682fdff94
Alexandr Galanov <alexandr.galanov@gmail.com>
2021-12-22 16:15:45 +0300

    Реализовано хранение хешированного пароля в базе. "Чистый" пароль больше нигде не хранится

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -1,8 +1,12 @@
+import hashlib
 from datetime import datetime
+from typing import Optional
 from uuid import uuid4
 
-from app import db
 from sqlalchemy.dialects.postgresql import UUID, BOOLEAN
+
+from app import db
+from config import SALT
 
 
 class User(db.Model):
@@ -27,6 +31,7 @@ class User(db.Model):
         :return:
         """
         user = User(**user_fields)
+        user.password = cls.password_hasher(user_fields['password'], SALT)
         if cls.is_user_exist(user_fields):
             return  # FIXME Что должен возвращать если пользователь уже есть?
         db.session.add(user)
@@ -34,19 +39,18 @@ class User(db.Model):
         return user
 
     @classmethod
-    def check_user(cls, user_fields: dict):
+    def check_user(cls, user_fields: dict) -> Optional[db.Model]:
         """
         Идентификация и аутентификация пользователя
 
         :param user_fields:
         :return:
         """
-        # FIXME здесь должна быть реализована проверка пароля, при условии что пароль мы в открытом виде не храним
         login = user_fields['login']
         password = user_fields['password']
         user = User.query.filter_by(login=login).one_or_none()
         if user:
-            if user.password == password:
+            if user.password == cls.password_hasher(password, SALT):
                 return user
             return
         return
@@ -55,7 +59,7 @@ class User(db.Model):
         return f'<User {self.login} {self.id}>'
 
     @classmethod
-    def is_user_exist(cls, user_fields: dict):
+    def is_user_exist(cls, user_fields: dict) -> bool:
         """
         Проверка на существование пользователя
 
@@ -68,3 +72,21 @@ class User(db.Model):
             return True
         return False
 
+    @classmethod
+    def password_hasher(cls, password: str, salt: str, hash_name: str = 'sha256', iterations: int = 100000,
+                        encoding: str = 'utf-8') -> str:
+        """
+        Создаёт хэш от пароля который будет храниться в базе
+        дока: https://docs.python.org/3.9/library/hashlib.html#hashlib.pbkdf2_hmac
+
+        :param password:
+        :param salt:
+        :param hash_name:
+        :param iterations:
+        :param encoding:
+        :return:
+        """
+
+        password_salted_hash = hashlib.pbkdf2_hmac(hash_name, password.encode(encoding), salt.encode(encoding),
+                                                   iterations)
+        return password_salted_hash
