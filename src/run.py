379b389c77732e0742a3488ed45b0457eb915a99
Alexandr Galanov <alexandr.galanov@gmail.com>
2021-12-29 21:53:03 +0300

    Добавлен порт в дебаг режиме на случай если понадобиться его переопределить

--- src/run.py
+++ src/run.py
@@ -15,7 +15,7 @@ jwt.init_app(app)
 
 if __name__ == "__main__":
     if "-d" in sys.argv:
-        app.run(host="0.0.0.0", debug=True)
+        app.run(host="0.0.0.0", port=SERVER_PORT, debug=True)
     else:
         http_server = WSGIServer((SERVER_HOST, SERVER_PORT), app)
         http_server.serve_forever()
