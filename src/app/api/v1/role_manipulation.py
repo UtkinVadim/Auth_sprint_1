f050d09aa94c480abf9884bec11d9d5ef28b6e21
Alexandr Galanov <alexandr.galanov@gmail.com>
2022-01-02 14:49:29 +0300

    Добавлена обработка на случай дубликата роли у пользователя

--- src/app/api/v1/role_manipulation.py
+++ src/app/api/v1/role_manipulation.py
@@ -19,11 +19,15 @@ class RoleManipulation(Resource):
     @jwt_with_role_required("admin")
     def post(self):
         args = role_parser.parse_args()
+        if models.UserRole.is_user_role_exists(args):
+            return {"message": "user already has role"}, HTTPStatus.CONFLICT
         models.UserRole.add(args["user_id"], args["role_id"])
         return {"message": "role added"}, HTTPStatus.OK
 
     @jwt_with_role_required("admin")
     def delete(self):
         args = role_parser.parse_args()
+        if not models.UserRole.is_user_role_exists(args):
+            return {"message": "user has no role"}, HTTPStatus.CONFLICT
         models.UserRole.delete(args["user_id"], args["role_id"])
         return {"message": "role deleted"}, HTTPStatus.OK
