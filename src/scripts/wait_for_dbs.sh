9f69eaa5e3e92c743104bca28d4637a73da67764
Alexandr Galanov <alexandr.galanov@gmail.com>
2022-01-02 10:18:12 +0300

    wait_for используется и в тестах и в боевом докере, поэтому переменные должны быть заданы общие

--- src/scripts/wait_for_dbs.sh
+++ src/scripts/wait_for_dbs.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 
-until nc -z "$TEST_DB_HOST" "$TEST_DB_PORT"; do
+until nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
   >&2 echo "Waiting for postgres..."
   sleep 1
 done
 
-until nc -z "$TEST_REDIS_HOST" "$TEST_REDIS_PORT"; do
+until nc -z "$REDIS_HOST" "$REDIS_PORT"; do
   >&2 echo "Waiting for redis..."
   sleep 1
 done
